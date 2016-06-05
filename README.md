834f262593564c5707a670da8be54f01901ede46
Matt Sergeant <matt@sergeant.org>
2016-06-05 15:50:30 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,23 @@
+# Outbound Rate Limits
+
+This plugin for Haraka implements outbound rate limits using redis
+as a shared source of the current rates.
+
+# Configuration
+
+Configuration is set in your `config/outbound_rate_limit.ini` file.
+
+The defaults are:
+
+```
+delay=30
+redis_host=127.0.0.1:6379
+
+[limits]
+;example.com=10
+```
+
+The counts for the limits are the maximum concurrency for a given outbound domain.
+
+The `delay` value are the number of seconds to delay before trying to send this
+mail again. The concurrency will be checked on every attempt to deliver the mail.
---
+++ index.js
@@ -0,0 +1,89 @@
+"use strict";
+
+var constants = require('haraka-constants');
+
+exports.redis_host = '127.0.0.1:6379';
+var redis_client;
+
+exports.register = function() {
+    var plugin = this;
+
+    plugin.load_config();
+    plugin.init_redis();
+
+    plugin.register_hook('send_email', 'increment_limit');
+    plugin.register_hook('delivered',  'decrement_limit');
+    plugin.register_hook('deferred',   'decrement_limit');
+    plugin.register_hook('bounce',     'decrement_limit');
+}
+
+exports.increment_limit = function (next, hmail) {
+    var plugin = this;
+
+    var rkey = 'outbound-rate-limit:' + hmail.domain;
+    redis_client.hincrby(rkey, 'TOTAL', 1);
+    redis_client.hget(rkey, 'TOTAL', function (err, count) {
+        if (err) {
+            plugin.logerror("Failed to get value from redis: " + err);
+            return next(); // just deliver
+        }
+        count = parseInt(count, 10);
+        if (plugin.cfg.limits[hmail.domain]) {
+            var limit = parseInt(plugin.cfg.limits[hmail.domain], 10);
+            if (limit && count > limit) {
+                return next(constants.delay, plugin.delay);
+            }
+        }
+        return next();
+    });
+}
+
+exports.decrement_limit = function (next, hmail) {
+    var plugin = this;
+
+    var rkey = 'outbound-rate-limit:' + hmail.domain;
+    redis_client.hincrby(rkey, 'TOTAL', -1);
+    return next();
+}
+
+exports.load_config = function () {
+    var plugin = this;
+
+    plugin.cfg = plugin.config.get('outbound_rate_limit.ini', {
+        booleans: [],
+    }, function () {
+        plugin.load_config();
+    });
+
+    plugin.delay = plugin.cfg.main.delay || 30;
+
+    if (plugin.cfg.main.redis_host &&
+        plugin.cfg.main.redis_host !== plugin.redis_host) {
+        plugin.redis_host = plugin.cfg.main.redis_host;
+        plugin.loginfo('set redis host to: ' + plugin.redis_host);
+    }
+};
+
+exports.init_redis = function () {
+    if (redis_client) { return; }
+
+    var redis = require('redis');
+    var host_port = this.redis_host.split(':');
+    var host = host_port[0] || '127.0.0.1';
+    var port = parseInt(host_port[1], 10) || 6379;
+
+    redis_client = redis.createClient(port, host);
+    redis_client.on('error', function (err) {
+        self.logerror('Redis error: ' + err);
+        redis_client.quit();
+        redis_client = null; // should force a reconnect
+        // not sure if that's the right thing but better than nothing...
+    });
+};
+
+exports.shutdown = function () {
+    clearInterval(this._interval);
+    if (redis_client) {
+        redis_client.end();
+    }
+};
